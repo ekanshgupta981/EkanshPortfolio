8e16089c1ffce2473b4d7d9afe90656cc9b47732
ekanshgupta981 <ekanshgurmon@gmail.com>
2023-04-23 23:39:26 +0530

    portfolio

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,6 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossorigin="anonymous"
         />
         <link
           rel="stylesheet"
@@ -21,7 +20,6 @@ export default function Document() {
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-          crossorigin="anonymous"
         ></script>
       </body>
     </Html>
